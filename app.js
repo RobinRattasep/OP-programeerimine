701c7cc802f0eb39a41df406a1386a513a6a3807
RobinRattasep <rattasep.robin@khk.ee>
2021-09-24 11:17:49 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("test");
+//COMMENT
+/*Comment
+two*/
+//Muutuja mille sisu saab muuta
+let name = "Robin Rattasep";
+
+name = "Kadi Lepp";
+
+let firstName = "Kadi"
+
+// muutuja mis on konstantne
+const surName = "Tamm";
+//surName = "Lepp";
+
+
+//massiivid
+const numbers = [1, 2, 3, 4];
+numbers.push(6);
+
+//objektid
+const person = {
+	nimi: "Kadi",
+	vanus: 20
+};
+
+person.vanus = 21;
+
+console.log(numbers)
+console.log(person);
