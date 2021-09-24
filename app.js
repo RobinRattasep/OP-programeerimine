26002d728eb8ee124776230742d065019abd1087
RobinRattasep <rattasep.robin@khk.ee>
2021-09-24 13:36:08 +0300

    Cincatenation

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 const firstName = "Kadi";
 const surName = "Tamm";
 const age = 20
-const text = "tere olen kadi";
 const tags = "arendus,veeb";
 
 let val;
 
-// concatenation
-val = firstName + " " + surName;
-//append
-val = firstName;
-val += surName;
-val = text + ", Minu töövaldkond on " + tags;
-
-
-//escaping
-val = "See on \"jutumärkides\"";
-
-//case
-val = firstName.toLowerCase()
-val = firstName.toUpperCase()
-
-val = surName[0];
-val = surName.charAt(2);
-val = surName.lenght;
-val = surName.charAt(surName.length - 1);
-
-val = surName.indexOf("a");
-val = surName.lastIndexOf("m");
-val = surName.indexOf("m");
-
-val = surName.substring(0, 2);
-val = surName.slice(0, 2);
-val = surName.slice(-3);
-
-val = tags.split(",")
+// concatenation {}
+val = `
+	<ul>
+		<li>Eesnimi: ${firstName}</li>
+		<li>Perenimi: ${surName}</li>
+		<li>Vanus: ${age}</li>
+		<li>Alad: ${tags}</li>
+	</ul>
+`;
 
 console.log(val);
+document.body.innerHTML = val;
