cc1ecabdedae43b04b0002d759f95c43fca0a896
RobinRattasep <rattasep.robin@khk.ee>
2021-09-24 13:21:45 +0300

    String operations

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-const num1 = 100;
-const num2 = 50;
+const firstName = "Kadi";
+const surName = "Tamm";
+const age = 20
+const text = "tere olen kadi";
+const tags = "arendus,veeb";
 
 let val;
 
-//Matemaatilised tehted
-val = num1 + num2;
-val = num1 - num2;
-val = num1 * num2;
-val = num1 / num2;
-val = num1 % num2;
-
-//math
-val = Math.PI
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(4, 2);
-
-val = Math.floor(Math.random() * 10);
+// concatenation
+val = firstName + " " + surName;
+//append
+val = firstName;
+val += surName;
+val = text + ", Minu töövaldkond on " + tags;
 
 
-console.log(val)
+//escaping
+val = "See on \"jutumärkides\"";
+
+//case
+val = firstName.toLowerCase()
+val = firstName.toUpperCase()
+
+val = surName[0];
+val = surName.charAt(2);
+val = surName.lenght;
+val = surName.charAt(surName.length - 1);
+
+val = surName.indexOf("a");
+val = surName.lastIndexOf("m");
+val = surName.indexOf("m");
+
+val = surName.substring(0, 2);
+val = surName.slice(0, 2);
+val = surName.slice(-3);
+
+val = tags.split(",")
+
+console.log(val);
