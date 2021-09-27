782e97554bdc03268e27ca1c101581b994974184
RobinRattasep <rattasep.robin@khk.ee>
2021-09-27 13:43:50 +0300

    Arrays and methods

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
-const numbers = [23, 24, 54, 65, 67, 3];
-const numbers2 = [15, 99, 27, 11, 22, 43];
-
-const fruits = ["õun", "banaan", "apelsin", "virsik"];
-
-
-const mixed = [22, "tere", true, undefined, null, {a: 1, b:2}, new Date()];
+// Objektide töötlus
+const person = {
+	firstname: "Kadi",
+	lastname: "Tamm",
+	age: 23,
+	email: "Kadi.Tamm@gmail.com",
+	hobbies: ["muusicka", "sport"],
+	address: {
+		city: "Tallinn",
+		country: "Harjumaa"
+	},
+	getBirthdayYear: function(){
+		return 2021 - this.age;
+	}
+};
 
 let val;
-val = numbers.length; //massiivi pikkus 
-val = Array.isArray(numbers); //kas on massiiv
-val = numbers[3]; // Kindel väärtus
-numbers[3] = 100;
-console.log(numbers);
+val = person.firstname;
+val = person["lastname"];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.city;
 
-//lisamine
-numbers.push(250);
-numbers.unshift(120);
-
-//maha võtmine
-numbers.pop();
-
-numbers.unshift();
-
-
-// index
-val = numbers.indexOf(100);
-
-//lõikamine
-numbers.splice(2, 1);
-
-//ühendamine / merge
-val = numbers.concat(numbers2);
-
-//sorteerimine
-val = fruits.sort();
-val = numbers.sort(function(x, y){
-	return y - x;
-});
+console.log(val);
