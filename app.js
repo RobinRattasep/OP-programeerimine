925dbedad7d0390e3bea03403dcc78a0585a403a
RobinRattasep <rattasep.robin@khk.ee>
2021-09-27 14:48:18 +0300

    If statements

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-const today = new Date();
-val = today.getMonth(); // Jaanurar - 0
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
-birthday = new Date("10/02/2021 12:00:42");
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(2020);
-birthday.setHours("17");
-birthday.setMinutes(30);
-birthday.setSeconds(40);
-birthday.setMilliseconds(15);
+/*
+if(tingumus){
+	//tegevused if puhul
+	
+}	else {
+	// tegevused else puhul
+}
+*/
+
+const id = 100;
+// võrdub - ==
+// võrdub + tüübikontroll - ===
+if(id == 100){
+	console.log("Okei");
+} else {
+	console.log("Ei ole okei");
+}
+let testId;
+if(typeof testId !== "undefined"){
+	console.log("ID is OK");
+} else { 
+console.log("ID does not exist");
+}
+
+// < ja >
+if(id>100){
+	console.log("ID is greater than 100");
+} else {
+	console.log("ID is equal or less than 100");
+}
+
+let color = "yellow";
+if(color == "red"){
+	console.log("STOP");
+}else if(color == "yellow"){
+	console.log("Attention");
+}else if(color =="green"){
+	console.log("GO");
+}
 
 
 
-
-
-
-console.log(birthday);
-
-console.log(val)
+let name = "Kadi";
+let age = 8;
+if(age > 0 && age <=12){
+	console.log(`${name} on lapse`)
+}
