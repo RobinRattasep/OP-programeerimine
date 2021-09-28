98e1b55d055b89394bb45c5111ac5e448ab6dda2
RobinRattasep <rattasep.robin@khk.ee>
2021-09-28 15:39:04 +0300

    Document and objeckt modeling

--- app.js
+++ app.js
@@ -1,45 +1,25 @@
-/*deklareerimine
-function greeting(firstname, lastname){
-	if(typeof firstname === "undefined"){
-		firstname = "Robin";
-	}
-	if(typeof lastname === "undefined"){
-		lastname = "Rattasep";
-	}
-	return "Tere, " + firstname + " " + lastname;
-}
-//kasutamine
-console.log(greeting())
+let val;
 
-// f-on defineeritud nagu const
-const square = function(number = 3){
-	return Math.pow(number, 2);
+val = document;
+val = document.all;
+val = document.all[4];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
 
-};
-console.log(square())
-*/
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].method;
+val = document.forms[0].action;
+val = document.forms[0].id;
 
-(function(){
-	console.log("Funktsion is working");
-})();
+val = document.links;
 
-(function(name){
-	console.log("Tere, " + name);
-})("Robin");
+val = document.scripts;
 
-const todoList = {
-	add: function(task){
-		console.log("Task is added");
-	},
-	edit: function(oldtask, newtask){
-		console.log(`${oldtask} on muudetud = ${newtask}`);
-	},
-	delete: function(task){
-		console.log(task + `is deleted`);
-	}
-};
-
-
-todoList.add("Study JS");
-todoList.edit("Studi JS", "Study Typescrpt");
-todoList.delete("Study JS");
+console.log(val)
