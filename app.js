c5a24e7687509a788688473da6c10264d1dff072
RobinRattasep <rattasep.robin@khk.ee>
2021-10-05 15:35:11 +0300

    Remove li elements

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
 form.addEventListener('submit', addTask);
+const taskList = document.querySelector("ul");
+const clearBtn = document.querySelector("#clear-tasks")
+
+taskList.addEventListener("click", removeTask);
+taskList.addEventListener("click", clearTask);
 
 function addTask(e) {
 	const li = document.createElement('li');
@@ -9,10 +14,29 @@ function addTask(e) {
 	li.appendChild(document.createTextNode(taskInput.value));
 	const link = document.createElement('a');
 	link.className = 'secondary-content';
-	link.appendChild(document.createTextNode('x'));
+	link.appendChild(document.createTextNode('X'));
 	link.setAttribute('href', '#');
-	li.appendChild(link);
+	li.appendChild(link);  
 	list.appendChild(li);
 	taskInput.value = '';
 	e.preventDefault();
 }
+
+function removeTask(e){
+	console.log("awdad")
+	if(e.target.textContent == "X"){
+		if(confirm("Kas tahad kustutda?")) {
+			e.target.parentElement.remove();
+		}
+	}
+}
+
+function clearTask(e){
+		console.log("adadakn");
+		if(confirm("Kas tahad kustutda?")) {
+			var myobj = document.getElementById("alltasks");
+			myobj.remove();
+		}
+}
+
+
